150cf1f97fe34ef6946bf0bd4747240600f47e1b
Timi <tunmisebabade@gmail.com>
2024-05-19 23:25:01 +0100

    first commit

--- src/pages/Feeds/components/trendingAds.jsx
+++ src/pages/Feeds/components/trendingAds.jsx
@@ -17,16 +17,17 @@ import 'react-loading-skeleton/dist/skeleton.css'
 const TrendingAds = () => {
     const { data, isLoading, error } = FetchTrendingAds();
     console.table(data?.data.normalads);
+    if (isLoading) return <Skeleton />
     return (
         <section className="overflow-x-hidden">
             <div className="flex items-center gap-2">
                 <img src={LOGO} className='w-10 h-10' alt="" />
-                <h1 className='font-bold lg:my-4 my-2 text-base md:text-3xl'>GET APARTMENTS NEAR YOU</h1>
+                <h1 className='font-bold lg:my-10 my-2 text-base md:text-3xl'>Find Your Dream Property: Discover Homes Tailored to Your Lifestyle!</h1>
             </div> 
             <section className="grid md:grid-cols-2 lg:grid-cols-4 gap-10">
                 {data?.data.normalads.filter((item) => item.categories === "Apartment").map((item) => (
                     <div key={item.id} className="flex flex-col gap-4">
-                        <div>
+                        <div className='border-2 rounded-md'>
                             <LazyLoadImage effect="blur" src={`https://apimypromospheretest.com.ng/public/storage/${item.titleImageurl.slice(7)}`} alt="" style={{ width: 400, height: 400, objectFit: "cover" }} className="rounded-md w-full h-full object-cover" />
                         </div>
                         <Link to="/profile/timilehin babade">
@@ -40,12 +41,12 @@ const TrendingAds = () => {
             </section>
             <div className="flex items-center gap-2">
                 <img src={LOGO} className='w-10 h-10' alt="" />
-                <h1 className='font-bold lg:my-4 my-2 text-base md:text-3xl'>ALWAYS STAY IN VOGUE</h1>
+                <h1 className='font-bold lg:my-10 my-2 text-base md:text-3xl'>ALWAYS STAY IN VOGUE</h1>
             </div>
             <section className="grid md:grid-cols-2 lg:grid-cols-4 gap-10">
                 {data?.data.normalads.filter((item) => item.categories === "Fashion").map((item) => (
                     <div key={item.id} className="flex flex-col gap-4">
-                        <div>
+                        <div className='border-2 rounded-md'>
                             <LazyLoadImage effect="blur" src={`https://apimypromospheretest.com.ng/public/storage/${item.titleImageurl.slice(7)}`} alt="" style={{ width: 400, height: 400, objectFit: "cover" }} className="rounded-md w-full h-full object-cover" />
                         </div>
                         <Link to="/profile/timilehin babade">
@@ -59,12 +60,12 @@ const TrendingAds = () => {
             </section>
             <div className="flex items-center gap-2">
                 <img src={LOGO} className='w-10 h-10' alt="" />
-                <h1 className='font-bold lg:my-4 my-2 text-base md:text-3xl'>YOU SKIN WILL THANK YOU</h1> 
+                <h1 className='font-bold lg:my-10 my-2 text-base md:text-3xl'>Unlock Your Best Skin: Elevate Your Routine with Premium Skincare!</h1> 
             </div>
             <section className="grid md:grid-cols-2 lg:grid-cols-4 gap-10">
                 {data?.data.normalads.filter((item) => item.categories === "Skincare").map((item) => (
                     <div key={item.id} className="flex flex-col gap-4">
-                        <div>
+                        <div className='border-2 rounded-md'>
                             <LazyLoadImage effect="blur"
                              src={`https://apimypromospheretest.com.ng/public/storage/${item.titleImageurl.slice(7)}`} alt="" style={{ width: 400, height: 400, objectFit: "cover" }} className="rounded-md w-full h-full object-cover" />
                         </div>
@@ -77,96 +78,26 @@ const TrendingAds = () => {
                     </div>
                 ))}
             </section>
-            {/* <section className="grid md:grid-cols-2 lg:grid-cols-4 gap-10">
-                <div className="flex flex-col gap-4">
-                    <div>
-                        <LazyLoadImage effect="blur" src={feed3} alt="" style={{ width: 400, height: 400, objectFit: "cover" }} className="rounded-md w-full h-full object-cover" />
-                    </div>
-                    <Link to="/profile/timilehin babade">
-                        <div className="flex items-center gap-2">
-                            <LazyLoadImage effect="blur" src={user} alt="" className="w-10 aspect-square" />
-                            <p className="text-sm">Timilehin babade</p>
+            <div className="flex items-center gap-2">
+                <img src={LOGO} className='w-10 h-10' alt="" />
+                <h1 className='font-bold lg:my-10 my-2 text-base md:text-3xl'>Unleash Your Productivity: Explore the Latest Laptop Innovations!</h1> 
+            </div>
+            <section className="grid md:grid-cols-2 lg:grid-cols-4 gap-10">
+                {data?.data.normalads.filter((item) => item.categories === "Laptops").map((item) => (
+                    <div key={item.id} className="flex flex-col gap-4">
+                        <div className='border-2 rounded-md'>
+                            <LazyLoadImage effect="blur"
+                             src={`https://apimypromospheretest.com.ng/public/storage/${item.titleImageurl.slice(7)}`} alt="" style={{ width: 400, height: 400, objectFit: "cover" }} className="rounded-md w-full h-full object-cover" />
                         </div>
-                    </Link>
-                </div>
-                <div className="flex flex-col gap-4">
-                    <div>
-                        <LazyLoadImage effect="blur" src={feed4} alt="" style={{ width: 400, height: 400, objectFit: "cover" }} className="rounded-md w-full h-full object-cover" />
+                        <Link to="/profile/timilehin babade">
+                            <div className="flex items-center gap-2">
+                                <LazyLoadImage effect="blur" src={item.user_image || user} alt="" className="rounded-full w-10 aspect-square" />
+                                <p className="text-sm">Timilehin babade</p>
+                            </div>
+                        </Link>
                     </div>
-                    <Link to="/profile/timilehin babade">
-                        <div className="flex items-center gap-2">
-                            <LazyLoadImage effect="blur" src={user} alt="" className="w-10 aspect-square" />
-                            <p className="text-sm">Timilehin babade</p>
-                        </div>
-                    </Link>
-                </div>
-                <div className="flex flex-col gap-4">
-                    <div>
-                        <LazyLoadImage effect="blur" src={feed5} alt="" style={{ width: 400, height: 400, objectFit: "cover" }} className="rounded-md w-full h-full object-cover" />
-                    </div>
-                    <Link to="/profile/timilehin babade">
-                        <div className="flex items-center gap-2">
-                            <LazyLoadImage effect="blur" src={user} alt="" className="w-10 aspect-square" />
-                            <p className="text-sm">Timilehin babade</p>
-                        </div>
-                    </Link>
-                </div>
-                <div className="flex flex-col gap-4">
-                    <div>
-                        <LazyLoadImage effect="blur" src={feed1} alt="" style={{ width: 400, height: 400, objectFit: "cover" }} className="rounded-md w-full h-full object-cover" />
-                    </div>
-                    <Link to="/profile/timilehin babade">
-                        <div className="flex items-center gap-2">
-                            <LazyLoadImage effect="blur" src={user} alt="" className="w-10 aspect-square" />
-                            <p className="text-sm">Timilehin babade</p>
-                        </div>
-                    </Link>
-                </div>
-                <div className="flex flex-col gap-4">
-                    <div>
-                        <LazyLoadImage effect="blur" src={feed1} alt="" style={{ width: 400, height: 400, objectFit: "cover" }} className="rounded-md w-full h-full object-cover" />
-                    </div>
-                    <Link to="/profile/timilehin babade">
-                        <div className="flex items-center gap-2">
-                            <LazyLoadImage effect="blur" src={user} alt="" className="w-10 aspect-square" />
-                            <p className="text-sm">Timilehin babade</p>
-                        </div>
-                    </Link>
-                </div>
-                <div className="flex flex-col gap-4">
-                    <div>
-                        <LazyLoadImage effect="blur" src={feed6} alt="" style={{ width: 400, height: 400, objectFit: "cover" }} className="rounded-md w-full h-full object-cover" />
-                    </div>
-                    <Link to="/profile/timilehin babade">
-                        <div className="flex items-center gap-2">
-                            <LazyLoadImage effect="blur" src={user} alt="" className="w-10 aspect-square" />
-                            <p className="text-sm">Timilehin babade</p>
-                        </div>
-                    </Link>
-                </div>
-                <div className="flex flex-col gap-4">
-                    <div>
-                        <LazyLoadImage effect="blur" src={feed2} alt="" style={{ width: 400, height: 400, objectFit: "cover" }} className="rounded-md w-full h-full object-cover" />
-                    </div>
-                    <Link to="/profile/timilehin babade">
-                        <div className="flex items-center gap-2">
-                            <LazyLoadImage effect="blur" src={user} alt="" className="w-10 aspect-square" />
-                            <p className="text-sm">Timilehin babade</p>
-                        </div>
-                    </Link>
-                </div>
-                <div className="flex flex-col gap-4">
-                    <div>
-                        <LazyLoadImage effect="blur" src={feed2} alt="" style={{ width: 400, height: 400, objectFit: "cover" }} className="rounded-md w-full h-full object-cover" />
-                    </div>
-                    <Link to="/profile/timilehin babade">
-                        <div className="flex items-center gap-2">
-                            <LazyLoadImage effect="blur" src={user} alt="" className="w-10 aspect-square" />
-                            <p className="text-sm">Timilehin babade</p>
-                        </div>
-                    </Link>
-                </div>
-            </section> */}
+                ))}
+            </section>
         </section>
     )
 }
