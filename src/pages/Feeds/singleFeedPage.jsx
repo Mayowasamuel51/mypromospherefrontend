3f2f00801c488bf5d0e903eb0edc51b86eee4b38
Timi <tunmisebabade@gmail.com>
2024-05-20 16:03:52 +0100

    first commit

--- src/pages/Feeds/singleFeedPage.jsx
+++ src/pages/Feeds/singleFeedPage.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import FetchSingleAd from '../../hooks/fetchSingleAd';
 import { Link, useParams, useNavigate } from 'react-router-dom';
 import Navbar from '../../components/Navbar';
 import Loader from '../../loader';
+import LOGO from "../../assests/SVGs/logo.svg"
 
 const SingleFeedPage = () => {
     const { id } = useParams();
     const navigate = useNavigate()
     const { data, isLoading, error } = FetchSingleAd(id);
-    console.table(data?.data?.data)
     if (isLoading) return <Loader />
     return (
         <>
             <Navbar blue={true} />
             <section className="pt-16 lg:pt-32 px-4 lg:px-10">
-                <div onClick={()=> navigate(-1)} className='cursor-pointer my-2'>
+                <div onClick={() => navigate(-1)} className='cursor-pointer my-2'>
                     Go Back
                 </div>
                 <div className="flex flex-col md:flex-row items-center md:items-start gap-4">
@@ -36,6 +35,26 @@ const SingleFeedPage = () => {
                         <p>Date Posted: {new Date(data?.data?.data?.created_at)?.toLocaleDateString()}</p>
                     </div>
                 </div>
+            </section>
+            <div className="px-4 lg:px-10 flex items-center gap-2 lg:my-10 my-5">
+                <img src={LOGO} className='w-10 h-10' alt="" />
+                <h1 className='font-medium md:font-bold text-sm lg:text-2xl exl:text-3xl'>OTHERS</h1>
+            </div>
+            <section className="px-4 lg:px-10 grid place-items-center md:grid-cols-2 lg:grid-cols-3 exl:grid-cols-4 gap-4">
+                {data?.data?.other_data.map((item) => (
+                    <Link to={`/feed/${item.id}`} key={item.id} className="flex flex-col gap-2 md:gap-4">
+                        <div className='border-2 rounded-md'>
+                            <LazyLoadImage effect="blur" src={`${item.itemadsimagesurls
+                                }`} alt="" style={{ width: 250, height: 250, objectFit: "cover" }} className="rounded-md" />
+                        </div>
+                        <Link to="/profile/timilehin babade">
+                            <div className="flex items-center gap-2">
+                                <LazyLoadImage effect="blur" src={item.user_image} alt="" className="rounded-full w-8 md:w-10 aspect-square border-2" />
+                                <p className="text-sm font-medium">USER-ID {item.user_id}</p>
+                            </div>
+                        </Link>
+                    </Link>
+                ))}
             </section>
         </>
     )
