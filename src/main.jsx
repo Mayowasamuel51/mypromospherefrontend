a37f3ad3aabac23f9c7d213f5b5fecca287f6b2b
mayowasamuel <fpasamuelmayowa51@gmail.com>
2024-06-08 08:24:20 +0100

    nedw

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,26 @@
 import { RouterProvider } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 // global styles
 import "./index.css";
 // routes/pages
 import { ContextProvider } from "./contexts/ContextProvider";
 import router from "./router";
 
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ContextProvider>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
-    </ContextProvider>
+    {/* <Analytics> */}
+      <ContextProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+          <Analytics />
+        </QueryClientProvider>
+      </ContextProvider>
+    {/* </Analytics> */}
   </React.StrictMode>
 );
-
-
-
-
-
