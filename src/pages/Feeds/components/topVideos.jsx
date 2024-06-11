6e1c99abfa692d74a66710d5d8d90ad2563a08fc
Timi <tunmisebabade@gmail.com>
2024-06-11 15:15:15 +0100

    first commit

--- src/pages/Feeds/components/topVideos.jsx
+++ src/pages/Feeds/components/topVideos.jsx
@@ -1,13 +1,6 @@
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
-import user from "../../../assests/images/user.svg"
 import anon from "../../../assests/images/anon.png"
-import feed1 from "../../../assests/images/feed1.svg";
-import feed2 from "../../../assests/images/feed2.svg";
-import feed3 from "../../../assests/images/feed3.svg";
-import feed4 from "../../../assests/images/feed4.svg";
-import feed5 from "../../../assests/images/feed5.svg";
-import feed6 from "../../../assests/images/feed6.svg";
 import { Link } from 'react-router-dom';
 import FetchVideos from '../../../hooks/fetchVideos';
 import ReactPlayer from 'react-player'
@@ -21,8 +14,8 @@ const TopVideos = () => {
         <section className="grid place-items-center md:grid-cols-2 lg:grid-cols-3 exl:grid-cols-4 gap-10 overflow-x-hidden">
             {data?.data?.videos.map((video) => (
                 <div key={video.id} className='flex flex-col gap-4'>
-                    <div>
-                        <ReactPlayer url={video?.titlevideourl} />
+                    <div className='border-2 border-red'>
+                        <ReactPlayer url={video?.titlevideourl} playing={true} light="true" loop={true} muted={true} />
                     </div>
                     <Link to={`/profile/user/${video.user_id}`} className="w-fit">
                         <div className="flex items-center gap-2">
