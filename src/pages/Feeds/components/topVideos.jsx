2b43047528f408f4e7df1a91ad97f5cc352fc4ae
Timi <tunmisebabade@gmail.com>
2024-06-12 16:31:47 +0100

    first commit

--- src/pages/Feeds/components/topVideos.jsx
+++ src/pages/Feeds/components/topVideos.jsx
@@ -8,7 +8,10 @@ import { useStateContext } from '../../../contexts/ContextProvider';
 import { FaPlay } from "react-icons/fa6";
 import { FaRegCirclePlay } from "react-icons/fa6";
 import Loader from '../../../loader';
-import thumbnail from "../../../assests/images/feed1.svg"
+import thumbnail1 from "../../../assests/images/feed1.svg"
+import thumbnail2 from "../../../assests/images/feed2.svg"
+import thumbnail3 from "../../../assests/images/feed3.svg"
+import thumbnail4 from "../../../assests/images/feed4.svg"
 
 const TopVideos = () => {
     const {token} = useStateContext();
@@ -20,8 +23,8 @@ const TopVideos = () => {
             {isLoading && <div className='md:col-span-2 lg:col-span-3 exl:col-span-4'><Loader /></div>}
             {data?.data?.videos.map((video) => (
                 <div key={video.id} className='flex flex-col gap-4'>
-                    <div className='w-full aspect-ratio-box'>
-                        <ReactPlayer width={280} height={300} style={{borderRadius: "8px"}} url={video?.titlevideourl} playing={true} light={thumbnail} loop={true} muted={true} playIcon={<FaRegCirclePlay size={40} color='#fff' />} className="w-fit rounded-lg hover:outline hover:outline-2 hover:outline-[#3D217A]" />
+                    <div className='w-full aspect-ratio-box rounded-lg overflow-hidden'>
+                        <ReactPlayer width={280} height={300} style={{borderRadius: "8px"}} url={video?.titlevideourl} playing={true} light={video.id % 2 === 0 ? thumbnail1 : thumbnail2} loop={true} muted={true} playIcon={<FaRegCirclePlay size={50} color='#fff' />} className="w-fit hover:outline hover:outline-2 hover:outline-[#3D217A]" />
                     </div>
                     <Link to={`/profile/user/${video.user_id}`} className="w-fit">
                         <div className="flex items-center gap-2">
