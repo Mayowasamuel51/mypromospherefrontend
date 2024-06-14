aaaee7e28c96a419200c0ab205ba16e8be2ab1cc
Timi <tunmisebabade@gmail.com>
2024-06-14 18:21:27 +0100

    first commit

--- src/pages/profile/components/profilePost.jsx
+++ src/pages/profile/components/profilePost.jsx
@@ -2,7 +2,7 @@ import Myuploads from "../../../testingpage/Myupload"
 import { useOutletContext } from "react-router-dom";
 
 const ProfilePost = () => {
-    const { id } = useOutletContext()
+    const id  = useOutletContext()
     console.log(id)
   return (
     <div className="px-4 lg:px-10 py-2 lg:py-10">
