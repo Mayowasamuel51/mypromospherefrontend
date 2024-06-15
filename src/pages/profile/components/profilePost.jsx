43e274d8828320198ff0000e3152be7c6a1a5469
Timi <tunmisebabade@gmail.com>
2024-06-16 00:58:19 +0100

    first commit

--- src/pages/profile/components/profilePost.jsx
+++ src/pages/profile/components/profilePost.jsx
@@ -15,16 +15,16 @@ const ProfilePost = () => {
   console.log(data)
   if (error) return <div className='min-h-screen grid place-items-center'><p><h1>{error?.message}</h1></p></div>
   return (
-    <div className="overflow-x-hidden">
+    <div className="overflow-x-hidden px-4 lg:px-10 py-2 lg:py-10">
       <section className="relative grid grid-cols-3 md:grid-cols-4 lg:grid-cols-4 exl:grid-cols-6 gap-4">
         {/* {isLoading && <div className='md:col-span-2 lg:col-span-3 exl:col-span-6'><Loader /></div>} */}
         {!data?.data.ads && <h1 className='text-center grid-cols-3 col-span-3 md:col-span-4 lg:col-span-4 exl:col-span-6'>{token?.id == id ? "You have" : "This User has"} not made any post Yet!</h1>}
         {(data?.data.ads && !isLoading) &&
           data?.data.ads.map((item) => (
-            <Link to={`/feed/${item.id}`} key={item.id} className="flex flex-col">
+            <Link to={`/feed/${item.id}`} key={item.id} className="flex flex-col border-2 border-red">
               <div className="">
                 <div className=''>
-                  <LazyLoadImage effect='blur' src={item.titleImageurl ? `https://apimypromospheretest.com.ng/public/storage/${item.titleImageurl.slice(7)}` : anon} alt="" className="w-full h-[100px] md:h-[200px] object-cover" />
+                  <LazyLoadImage effect='blur' src={`https://apimypromospheretest.com.ng/public/storage/${item.titleImageurl.slice(7)}`} alt="" className="w-full h-[100px] md:h-[200px] object-cover" />
                 </div>
               </div>
             </Link>
--- src/pages/profile/components/profileVideos.jsx
+++ src/pages/profile/components/profileVideos.jsx
@@ -22,7 +22,7 @@ const ProfileVideos = () => {
   console.log(data)
   if (error) return <div className='min-h-screen grid place-items-center'><p><h1>{error?.message}</h1></p></div>
   return (
-    <section className="relative grid md:gap-4 place-items-center md:grid-cols-2 lg:grid-cols-3 exl:grid-cols-4 gap-10 py-4">
+    <section className="relative grid md:gap-4 place-items-center md:grid-cols-2 lg:grid-cols-3 exl:grid-cols-4 gap-10 px-4 lg:px-10 py-2 lg:py-10">
       {isLoading && <div className='md:col-span-2 lg:col-span-3 exl:col-span-4'><Loader /></div>}
       {data?.data?.videos.map((video) => (
         <div key={video.id} className='flex flex-col gap-4'>
