fee94ed1ee1d438b33aa1cde657891eead884a46
Timi <tunmisebabade@gmail.com>
2024-06-16 01:45:47 +0100

    first commit

--- src/components/postsSkeleton.jsx
+++ src/components/postsSkeleton.jsx
@@ -2,14 +2,19 @@ import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css'
 import { useStateContext } from '../contexts/ContextProvider';
 
-const PostsSkeleton = () => {
-    const {FullScreen} = useStateContext()
-  return (
-    <div className='flex flex-col gap-2 md:gap-4'>
-        <Skeleton width={280} height={300} />
-        <Skeleton circle width={FullScreen ? 40 : 32} className="w-8 md:w-10 aspect-square" />
-    </div>
-  )
+const PostsSkeleton = ({ posts }) => {
+    const { FullScreen } = useStateContext()
+    return (
+        <div>
+            {Array.from({ length: posts }).map((_, index) => (
+                <div key={index} className='flex flex-col gap-2 md:gap-4'>
+                    <Skeleton width={280} height={300} />
+                    <Skeleton circle width={FullScreen ? 40 : 32} className="w-8 md:w-10 aspect-square" />
+                </div>
+            ))}
+        </div>
+
+    )
 }
 
 export default PostsSkeleton
