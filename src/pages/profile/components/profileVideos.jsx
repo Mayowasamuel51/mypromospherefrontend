83a04b6c4a927a2b6b82a7b3d5b50e5208b70b3c
Timi <tunmisebabade@gmail.com>
2024-06-16 05:25:05 +0100

    first commit

--- src/pages/profile/components/profileVideos.jsx
+++ src/pages/profile/components/profileVideos.jsx
@@ -12,7 +12,7 @@ import { FaRegCirclePlay } from "react-icons/fa6";
 import Loader from '../../../loader';
 import thumbnail1 from "../../../assests/images/feed1.svg"
 import thumbnail2 from "../../../assests/images/feed2.svg"
-import UploadSkeleton from "../../../components/uploadSkeleton";
+import VideoSkeleton from "../../../components/videoSkeleton"
 
 
 const ProfileVideos = () => {
@@ -29,7 +29,7 @@ const ProfileVideos = () => {
   return (
     <section className="relative grid md:gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 exl:grid-cols-6 gap-10 px-4 lg:px-10 py-2 lg:py-10">
     {(!data?.data.videos && !isLoading )&& <h1 className='text-center col-span-2 md:col-span-3 lg:col-span-4 exl:col-span-6 my-2'>{token?.id == id ? "You have" : "This User has"} not made any post Yet!</h1>}
-      {(isLoading) && <UploadSkeleton posts={6} />}
+      {(isLoading) && <VideoSkeleton posts={6} />}
       {data?.data?.videos.map((video) => (
         <div key={video.id} className='flex flex-col gap-4'>
           <div className='w-full aspect-ratio-box rounded-lg overflow-hidden'>
