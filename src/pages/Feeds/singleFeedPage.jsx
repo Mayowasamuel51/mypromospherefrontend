bdccdb4ba975cdbf38fd1aa3e5d4dddfb728ee07
Timi <tunmisebabade@gmail.com>
2024-06-16 02:40:57 +0100

    first commit

--- src/pages/Feeds/singleFeedPage.jsx
+++ src/pages/Feeds/singleFeedPage.jsx
@@ -25,12 +25,24 @@ const SingleFeedPage = () => {
                 </div>
                 <div className="flex flex-col md:flex-row md:items-start gap-4">
                     <div className="flex-1 flex flex-col gap-4">
-                        <div className='rounded-md'>
-                            <LazyLoadImage effect="blur" src={`https://apimypromospheretest.com.ng/public/storage/${data?.data?.data.titleImageurl.slice(7)}`} alt="img" className="rounded-md w-full h-full object-cover" />
+                        <div className='relative'>
+                            <div className='rounded-md'>
+                                <LazyLoadImage effect="blur" src={`https://apimypromospheretest.com.ng/public/storage/${data?.data?.data.titleImageurl.slice(7)}`} alt="img" className="rounded-md w-full h-[300px] md:h-[400px] object-cover" />
+                            </div>
+                            <div className="flex gap-2 absolute top-2 right-2">
+                                {data?.data?.other_data.map((item) => (
+                                    <div to={`/feed/${item.id}`} key={item.id} className="flex flex-col gap-2 md:gap-4">
+                                        <div className='border-2 rounded-md'>
+                                            <LazyLoadImage effect="blur" src={`${item.itemadsimagesurls
+                                                }`} alt="" style={{ width: 30, height: 30, objectFit: "cover" }} className="rounded-md" />
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
                         <Link to={`/profile/user/${data?.data?.data.user_id}`}>
                             <div className="flex items-center gap-2">
-                                <img src={data?.data?.data.user_image ?? anon} alt="user-profile" className="rounded-full w-10 aspect-square" />
+                                <img src={data?.data?.data.user_image === "null" || data?.data?.data.user_image ? anon : data?.data?.data.user_image} alt="user-profile" className="rounded-full w-10 aspect-square" />
                             </div>
                         </Link>
                     </div>
@@ -54,26 +66,6 @@ const SingleFeedPage = () => {
                         </div>
                     </div>
                 </div>
-            </section>
-            <div className="px-4 lg:px-10 flex items-center gap-2 lg:my-10 my-5">
-                <img src={LOGO} className='w-10 h-10' alt="" />
-                <h1 className='font-medium md:font-bold text-sm lg:text-2xl exl:text-3xl'>OTHERS</h1>
-            </div>
-            <section className="px-4 lg:px-10 grid place-items-center md:grid-cols-2 lg:grid-cols-3 exl:grid-cols-4 gap-4">
-                {data?.data?.other_data.map((item) => (
-                    <Link to={`/feed/${item.id}`} key={item.id} className="flex flex-col gap-2 md:gap-4">
-                        <div className='border-2 rounded-md'>
-                            <LazyLoadImage effect="blur" src={`${item.itemadsimagesurls
-                                }`} alt="" style={{ width: 250, height: 250, objectFit: "cover" }} className="rounded-md" />
-                        </div>
-                        <Link to="/profile/timilehin babade">
-                            <div className="flex items-center gap-2">
-                                <LazyLoadImage effect="blur" src={item.user_image} alt="" className="rounded-full w-8 md:w-10 aspect-square border-2" />
-                                {/* <p className="text-sm font-medium">USER-ID {item.user_id}</p> */}
-                            </div>
-                        </Link>
-                    </Link>
-                ))}
             </section>
         </>
     )
