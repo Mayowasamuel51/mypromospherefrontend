653f9a09a9ce44177d842cd8aed760f7a2bce319
Timi <tunmisebabade@gmail.com>
2024-06-18 23:38:47 +0100

    first commit

--- src/components/postsSkeleton.jsx
+++ src/components/postsSkeleton.jsx
@@ -7,7 +7,7 @@ const PostsSkeleton = ({ posts }) => {
     return  Array.from({ length: posts }).map((_, index) => (
         <div key={index}   className='flex flex-col gap-2 md:gap-4'>
             <div>
-                <Skeleton baseColor={isDarkMode && "#27272c"} highlightColor={isDarkMode && "#444"}  width={280} height={300} />
+                <Skeleton baseColor={isDarkMode && "#27272c"} highlightColor={isDarkMode && "#444"}  width={`100%`} height={300} />
             </div>
             <div>
                 <Skeleton baseColor={isDarkMode && "#27272c"} highlightColor={isDarkMode && "#444"} circle width={FullScreen ? 40 : 32} className="w-8 md:w-10 aspect-square" />
