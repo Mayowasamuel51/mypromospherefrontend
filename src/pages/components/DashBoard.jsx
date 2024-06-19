9e3709bb9cce70a60dfad027285817ad74686a70
Timi <tunmisebabade@gmail.com>
2024-06-19 08:05:22 +0100

    first commit

--- src/pages/components/DashBoard.jsx
+++ src/pages/components/DashBoard.jsx
@@ -20,7 +20,7 @@ function DefualtLayout() {
         <>
             <main className={`px-4 md:px-10`}>
                 <section className="flex items-start md:gap-10 gap-4">
-                    <article className="rounded-md bg-[#3D217A] md:rounded-none md:bg-white w-fit flex text-center md:text-start flex-col justify-between gap-20 py-2 px-2 md:py-10 md:px-6">
+                    <article className="rounded-md bg-[#3D217A] md:rounded-none md:bg-white w-fit flex items-center md:items-start text-center md:text-start flex-col justify-between gap-20 py-2 px-2 md:py-10 md:px-6">
                         <div className="flex flex-col md:gap-6 gap-4">
                             <div className="flex flex-col gap-2">
                                 <img
@@ -33,19 +33,19 @@ function DefualtLayout() {
                                 </h1>
                                 <p className="text-xs text-slate-400 md:block hidden">{token ? `${token["user"]}` : 'Anonymous'}</p>
                             </div>
-                            <div className="flex flex-col md:gap-6 gap-4 justify-center">
+                            <div className="flex items-center md:items-start flex-col md:gap-6 gap-4 justify-center">
                                 <button className="flex items-center gap-2 duration-200 rounded-md cursor-pointer">
-                                    <FaShare size={20}/>
+                                    <FaShare size={20} className="text-white md:text-black"/>
                                     <p className="text-center md:block hidden">share</p>
                                 </button>
                                 <Link to={'EditProfile'} className="">
                                     <button className="flex items-center gap-2 duration-200rounded-md cursor-pointer">
-                                        <FaEdit size={20} />
+                                        <FaEdit size={20} className="text-white md:text-black" />
                                         <p className="text-center md:block hidden">Edit profile</p>
                                     </button>
                                 </Link>
                             </div>
-                            <div className="flex flex-col md:gap-6 gap-4 justify-center gap-x-6">
+                            <div className="flex items-center md:items-start flex-col md:gap-6 gap-4 justify-center gap-x-6">
                                 <p onClick={()=> setSaved(false)} className={`${!saved && "font-bold text-white md:text-[#3D217A]"} cursor-pointer flex items-center gap-2`}>
                                     <MdDynamicFeed size={20} />
                                     <span className="md:block hidden">Post</span>
@@ -57,7 +57,7 @@ function DefualtLayout() {
                             </div>
                         </div>
                         <Link to={`post`}>
-                            <button className="text-[#3D217A] cursor-pointer flex items-center gap-2">
+                            <button className="text-white md:text-black cursor-pointer flex items-center gap-2">
                                 <FiPlusSquare size={20} />
                                 <p className="md:block hidden">Post an Ad</p>
                             </button>
