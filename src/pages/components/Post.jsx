723839a30dbfab9b2fdf25152ee12112f583db01
Timi <tunmisebabade@gmail.com>
2024-06-19 04:56:53 +0100

    first commit

--- src/pages/components/Post.jsx
+++ src/pages/components/Post.jsx
@@ -4,9 +4,7 @@ const Post = () => {
   return (
     <div>
       <div className="">
-        <div className="px-4 lg:px-10 py-2 lg:py-10">
-          <Myuploads />
-        </div>
+        <Myuploads />
       </div>
     </div>
   );
