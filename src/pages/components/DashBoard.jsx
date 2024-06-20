94a40f4aea42caf987ca56aa3196f6a3e6e1d820
Timi <tunmisebabade@gmail.com>
2024-06-20 13:11:02 +0100

    first commit

--- src/pages/components/DashBoard.jsx
+++ src/pages/components/DashBoard.jsx
@@ -46,7 +46,7 @@ function Dashboard() {
             <DashBoardNav />
             <main className={`px-4 md:px-10 py-20 md:py-28`}>
                 <section className="flex items-start md:gap-10 gap-4">
-                    <motion.article variants={controlsVariant} animate={hidden ? "animate" : "initial"} className="top-20 left-4 fixed md:hidden z-10 rounded-md bg-[#3D217A] bg-opacity-90 md:rounded-none w-fit flex items-center md:items-start text-center md:text-start flex-col justify-between gap-20 py-2 px-2 md:py-10 md:px-6">
+                    <motion.article variants={controlsVariant} animate={hidden ? "animate" : "initial"} className="top-20 left-4 fixed md:hidden z-10 rounded-md bg-[#3D217A] bg-opacity-90 md:rounded-none w-fit flex items-center md:items-start text-center md:text-start flex-col justify-between gap-20 py-3 px-2 md:px-6">
                         <div className="flex flex-col md:gap-6 gap-5">
                             <div className="flex flex-col gap-2">
                                 <img
