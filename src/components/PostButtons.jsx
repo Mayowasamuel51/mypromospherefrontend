44880f659515f1bed34442bf7f6b5f29c4358a13
Timi <tunmisebabade@gmail.com>
2024-06-20 23:24:01 +0100

    first commit

--- src/components/PostButtons.jsx
+++ src/components/PostButtons.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 function PostButtons() {
   return (
     <div className="my-4 shadow-md md:p-4 w-fit mx-auto overflow-hidden bg-[#F0D8DD]">
-      <div className="flex justify-center items-center gap-2 md:gap-10 font-medium">
+      <div className="flex justify-center items-center md:gap-10 font-medium">
         <Link to="/dashboard/postAd">
           <motion.button whileTap={{ scale: 1.05 }} className={`px-2 md:px-6 py-2 md:py-4 md:text-base text-xs ${location.pathname === "/dashboard/postAd" && "bg-[#EC6A87] text-white"}`}>Post a Top Ad</motion.button>
         </Link>
