e0060c34e309dd84b3eb7a3a429b1e8c41c777e0
Timi <tunmisebabade@gmail.com>
2024-06-20 19:15:58 +0100

    first commit

--- src/pages/Feeds/feedsHome.jsx
+++ src/pages/Feeds/feedsHome.jsx
@@ -37,7 +37,7 @@ const FeedsHome = () => {
       toast.error("You are not Logged In")
       navigate('/login');
     } else {
-      navigate('/dashboard/postAds');
+      navigate('/dashboard/postAd');
     }
   }
 
