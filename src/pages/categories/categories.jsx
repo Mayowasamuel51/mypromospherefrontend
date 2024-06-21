b110a3f3886c8ba7080247dbb4bc8330cad95370
Timi <tunmisebabade@gmail.com>
2024-06-21 14:15:43 +0100

    first commit

--- src/pages/categories/categories.jsx
+++ src/pages/categories/categories.jsx
@@ -15,7 +15,7 @@ const Categories = () => {
   return (
     <>
       <Navbar blue={true} />
-      <section className="py-20 lg:py-20 px-4">
+      <section className="py-20 lg:py-20 px-4 md:px-10">
         {category === "discount" && <h1 className="font-semibold text-2xl capitalize mx-3 md:mx-8 my-4">Products/properties on discount</h1>}
         {error && <div className='min-h-screen grid place-items-center text-red md:text-xl text-lg'><p>{error?.message}</p></div>}
         <div className="relative grid md:grid-cols-2 lg:grid-cols-3 exl:grid-cols-4 gap-4">
