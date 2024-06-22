04cb255f389e1887c790a86794becb96b46649de
Timi <tunmisebabade@gmail.com>
2024-06-22 06:34:19 +0100

    first commit

--- src/pages/Feeds/feedsHome.jsx
+++ src/pages/Feeds/feedsHome.jsx
@@ -164,8 +164,8 @@ const FeedsHome = () => {
               </div>
             </section>
             <AnimatePresence mode='popLayout'>
-              <motion.div variants={containerVariant} animate={(modal) ? "animate" : "initial"} className="border-2 border-red fixed inset-0 flex justify-center items-center bg-black bg-opacity-80 z-[99999999]">
-                {(modal) &&
+              <motion.div variants={containerVariant} animate={(modal && searchResults?.data) ? "animate" : "initial"} className="border-2 border-red fixed inset-0 flex justify-center items-center bg-black bg-opacity-80 z-[99999999]">
+                {(modal && searchResults?.data) &&
                   <motion.div variants={divVariant} className="w-fit bg-white p-4 rounded-md flex items-center gap-4">
                     {searchResults?.data.map((item) => (
                       <motion.div variants={childVariant} key={item.id} className="flex-1 flex flex-col gap-2 md:gap-4">
