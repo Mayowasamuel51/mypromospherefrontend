2e097f01a6b5b2f14a733f4cf7b83ed320a70b1f
Timi <tunmisebabade@gmail.com>
2024-06-22 16:55:57 +0100

    first commit

--- src/pages/Feeds/feedsHome.jsx
+++ src/pages/Feeds/feedsHome.jsx
@@ -147,7 +147,7 @@ const FeedsHome = () => {
               <div className="flex my-3 lg:my-3">
                 <ReactSearchAutocomplete
                   items={categories}
-                  className="z-[9999999] w-full lg:w-[80%] md:border-none focus:shadow-none h-10 lg:h-12 "
+                  className="z-[20] w-full lg:w-[80%] md:border-none focus:shadow-none h-10 lg:h-12 "
                   placeholder="Search by title or tags , service"
                   onSearch={handleOnSearch}
                   onSelect={handleOnSelect}
