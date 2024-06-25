5a71e958c86bf3a08d70e272721b3a04ac8bc4a8
Timi <tunmisebabade@gmail.com>
2024-06-25 02:32:27 +0100

    first commit

--- src/components/links.jsx
+++ src/components/links.jsx
@@ -1,9 +1,9 @@
 import { FaChevronDown } from "react-icons/fa";
-import { Link, useLocation } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { FiChevronDown } from "react-icons/fi";
 import { AnimatePresence, motion } from "framer-motion"
-
+import PropTypes from 'prop-types';
 
 const Links = ({hidden, bg}) => {
     const [selected, setSelected] = useState(null);
@@ -43,14 +43,14 @@ const Links = ({hidden, bg}) => {
                     {selected && <Content dir={dir} selected={selected} />}
                 </AnimatePresence>
             </div>
-            <Link className={`nav-active px-2 py-1.5`} to="/category/discount">Products on Discount</Link>
-            <Link className={`nav-active px-2 py-1.5`} to="/category/Kids_Baby_dresses">Baby Products</Link>
+            <NavLink to="/category/discount" className={({isActive})=> isActive ? "font-black nav-active px-2 py-1.5" : "nav-active px-2 py-1.5"}>Products on Discount</NavLink>
+            <NavLink className={`nav-active px-2 py-1.5`} to="/category/Kids_Baby_dresses">Baby Products</NavLink>
         </ul>
     )
 }
 
 const Tab = ({ children, tab, handleSetSelected, selected, bg, hidden }) => {
-    const { pathname } = useLocation()
+    // const { pathname } = useLocation()
     return (
         <button
             id={`shift-tab-${tab}`}
@@ -85,14 +85,14 @@ const Content = ({ selected, dir }) => {
                 opacity: 0,
                 y: 8,
             }}
-            className="absolute left-0 top-[calc(100%_+_24px)] w-96 rounded-lg border bg-white shadow-md p-4"
+            className="absolute left-0 top-[calc(100%_+_24px)] w-96 rounded-lg border bg-white dark:bg-darkBg shadow-md p-4"
         >
             <Bridge />
             <Nub selected={selected} />
 
             {TABS.map((T) => {
                 return (
-                    <div className="overflow-hidden text-black font-medium leading-9" key={T.id}>
+                    <div className="overflow-hidden text-black dark:bg-darkGray font-medium leading-9" key={T.id}>
                         {selected === T.id && (
                             <motion.div
                                 initial={{
@@ -146,7 +146,7 @@ const Nub = ({ selected }) => {
             }}
             animate={{ left }}
             transition={{ duration: 0.25, ease: "easeInOut" }}
-            className="absolute left-1/2 top-0 h-4 w-4 -translate-x-1/2 -translate-y-1/2 rotate-45 rounded-tl border bg-white"
+            className="absolute left-1/2 top-0 h-4 w-4 -translate-x-1/2 -translate-y-1/2 rotate-45 rounded-tl border bg-white dark:bg-darkBg"
         />
     );
 };
@@ -154,10 +154,10 @@ const Nub = ({ selected }) => {
 const SubHot = () => {
     return (
         <ul className="flex flex-col">
-            <Link to="/category/property">Property</Link>
-            <Link to="/category/apartment">Apartment</Link>
-            <Link to="/category/cars">Cars</Link>
-            <Link to="/category/laptops">Laptops</Link>
+            <NavLink to="/category/property">Property</NavLink>
+            <NavLink to="/category/apartment">Apartment</NavLink>
+            <NavLink to="/category/cars">Cars</NavLink>
+            <NavLink to="/category/laptops">Laptops</NavLink>
         </ul>
     )
 }
@@ -165,9 +165,9 @@ const SubHot = () => {
 const SubFashion = () => {
     return (
         <ul className="flex flex-col">
-            <Link to="/category/womens-shirts">Womens-shirts</Link>
-            <Link to="/category/womens-watches">Womens-watches</Link>
-            <Link to="/category/mens-watches">Mens-watches</Link>
+            <NavLink to="/category/womens-shirts">Womens-shirts</NavLink>
+            <NavLink to="/category/womens-watches">Womens-watches</NavLink>
+            <NavLink to="/category/mens-watches">Mens-watches</NavLink>
         </ul>
     )
 }
@@ -185,5 +185,28 @@ const TABS = [
         icon: <FaChevronDown />
     }
 ].map((n, idx) => ({ ...n, id: idx + 1 }));
+
+Links.propTypes = {
+    hidden : PropTypes.boolean,
+    bg : PropTypes.boolean,
+}
+
+Tab.propTypes = {
+    children:  PropTypes.any,
+    tab:  PropTypes.any, 
+    handleSetSelected:  PropTypes.any, 
+    selected:  PropTypes.any,
+    hidden : PropTypes.boolean,
+    bg : PropTypes.boolean,
+}
+
+Content.propTypes = {
+    selected:  PropTypes.any,
+    dir : PropTypes.any,
+}
+
+Nub.propTypes = {
+    selected:  PropTypes.any,
+}
 
 export default Links
