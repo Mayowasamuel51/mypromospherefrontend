4d6baf66f8d8761b72019be0904a2746971d5579
Timi <tunmisebabade@gmail.com>
2024-06-25 02:57:44 +0100

    first commit

--- src/pages/Feeds/components/feedBack.jsx
+++ src/pages/Feeds/components/feedBack.jsx
@@ -1,8 +1,59 @@
+import { motion } from 'framer-motion';
+import { useStateContext } from "../../../contexts/ContextProvider";
 
-const FeedBack = () => {
-  return (
-    <div>FeedBack</div>
-  )
+const containerVariant = {
+    initial: {
+        opacity: 0,
+        zIndex: -1
+    },
+    animate: {
+        opacity: 1,
+        zIndex: 99999999,
+        transition: {
+            duration: 0.5, delayChildren: 0.4
+        }
+    },
+    exit: {
+        opacity: 0,
+        zIndex: -1,
+        transition: {
+            when: "afterChildren", staggerChildren: 0.3, duration: 0.3
+        }
+    }
 }
 
-export default FeedBack
+const divVariant = {
+    initial: {
+        opacity: 0,
+        y: "-100%",
+    },
+    animate: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            type: "spring", staggerChildren: 0.3, delayChildren: 0.5, duration: 0.5, stiffness: 250
+        }
+    },
+    exit: {
+        opacity: 0,
+        y: "-100%",
+        transition: {
+            when: "afterChildren", duration: 0.3, staggerChildren: 0.3,
+        }
+    }
+}
+
+const FeedBack = () => {
+    const { token } = useStateContext();
+    return (
+        <motion.section variants={containerVariant} initial="initial" animate="animate" exit="exit" className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-80">
+            <motion.div variants={divVariant} className="flex flex-col gap-2 w-[80%] md:w-[750px] bg-white dark:bg-darkBg py-4 px-4 md:px-6 rounded-md">
+                {!token && <input className="w-full md:h-14 h-12" type="text" />}
+                <textarea name="" id="" className="w-full resize-none md:h-32"></textarea>
+                <button>Send FeedBack</button>
+            </motion.div>
+        </motion.section>
+    )
+}
+
+export default FeedBack;
