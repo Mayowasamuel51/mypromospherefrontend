7f8967e7b4f78c8afa55fb71232a4551b5537b18
Timi <tunmisebabade@gmail.com>
2024-06-25 02:49:42 +0100

    first commit

--- src/pages/Feeds/singleFeedPage.jsx
+++ src/pages/Feeds/singleFeedPage.jsx
@@ -12,10 +12,51 @@ import { useStateContext } from '../../contexts/ContextProvider';
 import { motion, AnimatePresence } from 'framer-motion';
 import { FaHeart } from "react-icons/fa";
 
+const containerVariant = {
+    initial: {
+      opacity: 0,
+      zIndex: -1
+    },
+    animate: {
+      opacity: 1,
+      zIndex: 99999999,
+      transition: {
+        duration: 0.5, delayChildren: 0.4
+      }
+    },
+    exit: {
+      opacity: 0,
+      zIndex: -1,
+      transition: {
+        when: "afterChildren", staggerChildren: 0.3, duration: 0.3
+      }
+    }
+  }
+  
+  const divVariant = {
+    initial: {
+      opacity: 0,
+      y: "-100%",
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring", staggerChildren: 0.3, delayChildren: 0.5, duration: 0.5, stiffness: 250
+      }
+    },
+    exit: {
+      opacity: 0,
+      y: "-100%",
+      transition: {
+        when: "afterChildren", duration: 0.3, staggerChildren: 0.3,
+      }
+    }
+  }
+
 const SingleFeedPage = () => {
     const [comment, setComment] = useState(false);
-
-    const { FullScreen } = useStateContext()
+    const { FullScreen, token } = useStateContext()
     const { id } = useParams();
     const navigate = useNavigate()
     const { data, isLoading, error } = FetchSingleAd(id);
@@ -23,6 +64,10 @@ const SingleFeedPage = () => {
     
     const [imageUrl, setImageUrl] = useState("")
     const [imageKey, setImageKey] = useState(0);
+
+    const handleFeedBack = ()=> {
+        setComment(prev=> !prev)
+    }
 
 
     useEffect(() => {
@@ -106,7 +151,7 @@ const SingleFeedPage = () => {
                             </div>
                         </div>
                         <div className='flex items-center gap-5 md:gap-10'>
-                            <button className="flex-1 bg-purple border border-purple hover:bg-transparent hover:text-purple duration-300 w-full py-2 md:py-3 px-4 text-white rounded-md">Leave a Feedback</button>
+                            <button onClick={()=> handleFeedBack()} className="flex-1 bg-purple border border-purple hover:bg-transparent hover:text-purple duration-300 w-full py-2 md:py-3 px-4 text-white rounded-md">Leave a Feedback</button>
                             <div>
                                 <FaHeart color="red" size={30} />
                             </div>
@@ -114,6 +159,17 @@ const SingleFeedPage = () => {
                     </div>
                 </div>
             </section>
+
+            <AnimatePresence>
+                {comment &&
+                <motion.section variants={containerVariant} initial="initial" animate="animate" exit="exit" className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-80">
+                    <motion.div variants={divVariant} className="flex flex-col gap-2 w-[80%] md:w-[750px] bg-white dark:bg-darkBg py-4 px-4 md:px-6 rounded-md">
+                        {!token && <input className="w-full md:h-14 h-12" type="text" />}
+                        <textarea name="" id="" className="w-full resize-none md:h-32"></textarea>
+                        <button>Send FeedBack</button>
+                    </motion.div>
+                </motion.section>}
+            </AnimatePresence>
         </>
     )
 }
